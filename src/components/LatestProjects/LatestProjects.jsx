3a2165b69c99a8246f3e06aed1cfc5a8333d5d21
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-02-20 06:41:03 -0800

    new code added

--- src/components/LatestProjects/LatestProjects.jsx
+++ src/components/LatestProjects/LatestProjects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import equidefi from '../../assets/myskills/images.jpg';
-import sensa from '../../assets/myskills/sensa.png';
+import sensa from '../../assets/myskills/download.jpg';
 import './LatestProjects.css'
 
 const LatestProjects = () => {
@@ -16,18 +16,20 @@ const LatestProjects = () => {
                         <span>WEB DEVELOPMENT</span>
                         <h2>Website Development for Investing Portal </h2>
                         <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores</p>
-                        <a href=''>VIEW PROJECT</a>
+                        <a href='https://github.com/rahmanabdur1/equidefi'>VIEW PROJECT</a>
+                        <a href='https://equidefi.com/'>VIEW CODE</a>
                     </div>
                 </div>
                 <div className='project-content sensa '>
                     <div className='project-text'>
                     <span>WEB DEVELOPMENT</span>
-                        <h2>Website Development for Your Calm Mnd Assistant </h2>
+                        <h2>Website Development for Your MIND Health </h2>
                         <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores</p>
-                        <a href=''>VIEW PROJECT</a>
+                        <a href='https://github.com/rahmanabdur1/sensa-health'>VIEW PROJECT</a>
+                        <a href='https://sensa.health/'>VIEW  CODE</a>
                     </div>
                     <div className='project-img'>
-                    <img src={equidefi} alt='' />
+                    <img src={sensa} alt='' />
                     </div>
                 </div>
             </div>
